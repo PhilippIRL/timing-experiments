f10845e885b6173cdaf01f957fe1a2a8692ab8a6
Philipp PplusS <pplussinfo@gmail.com>
2020-08-06 01:55:04 +0200

    first commit

---
+++ app/src/main/java/de/ppluss/timingexperiments/MainActivity.java
@@ -0,0 +1,164 @@
+package de.ppluss.timingexperiments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SeekBar fireBar, waitBar;
+    private ToggleButton loopButton;
+    private TextView fireLabel, waitLabel, modeLabel;
+    private Spinner modeSelect;
+    private Button singleRunButton;
+
+    private final int maxValue = 1000;
+
+    private int waitTime = 100, fireTime = 100, mode = 0;
+
+    private WorkerThread workerThread = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.fireBar = findViewById(R.id.fireBar);
+        this.waitBar = findViewById(R.id.waitBar);
+        this.loopButton = findViewById(R.id.loopButton);
+        this.fireLabel = findViewById(R.id.fireLabel);
+        this.waitLabel = findViewById(R.id.waitLabel);
+        this.modeLabel = findViewById(R.id.modeLabel);
+        this.modeSelect = findViewById(R.id.modeSelect);
+        this.singleRunButton = findViewById(R.id.singleRunButton);
+
+        waitBar.setMax(maxValue);
+        fireBar.setMax(maxValue);
+
+        waitBar.setProgress(waitTime);
+        fireBar.setProgress(fireTime);
+        modeSelect.setSelection(mode);
+        modeSelect.setEnabled(true);
+        singleRunButton.setEnabled(true);
+
+        updateFireLabel();
+        updateWaitLabel();
+        updateModeLabel();
+
+        SeekBar.OnSeekBarChangeListener changeListener = new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(seekBar.equals(waitBar)) {
+                    waitTime = progress;
+                    if(workerThread != null) {
+                        workerThread.waitTime = progress;
+                    }
+                    updateWaitLabel();
+                } else if(seekBar.equals(fireBar)) {
+                    fireTime = progress;
+                    if(workerThread != null) {
+                        workerThread.fireTime = progress;
+                    }
+                    updateFireLabel();
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+
+        };
+
+        waitBar.setOnSeekBarChangeListener(changeListener);
+        fireBar.setOnSeekBarChangeListener(changeListener);
+
+        modeSelect.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                mode = position;
+                updateModeLabel();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+
+        });
+
+        workerThread = new WorkerThread(this);
+        workerThread.start();
+
+        loopButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    modeSelect.setEnabled(false);
+                    singleRunButton.setEnabled(false);
+
+                    workerThread.mode = mode;
+                    workerThread.fireTime = fireTime;
+                    workerThread.waitTime = waitTime;
+
+                    workerThread.shouldWork = true;
+
+                } else {
+
+                    workerThread.shouldWork = false;
+                    workerThread.stopEverything();
+
+                    modeSelect.setEnabled(true);
+                    singleRunButton.setEnabled(true);
+                }
+            }
+
+        });
+
+        singleRunButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                workerThread.mode = mode;
+                workerThread.fireTime = fireTime;
+                workerThread.waitTime = waitTime;
+
+                workerThread.singleRun = true;
+            }
+        });
+
+    }
+
+    private void updateModeLabel() {
+        String[] modesInText = getResources().getStringArray(R.array.modesInText);
+        modeLabel.setText(getString(R.string.modeText, modesInText[this.mode]));
+    }
+
+    private void updateFireLabel() {
+        fireLabel.setText(getString(R.string.fireText, fireTime));
+    }
+
+    private void updateWaitLabel() {
+        waitLabel.setText(getString(R.string.waitText, waitTime));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        workerThread.shouldWork = false;
+        workerThread.stopEverything();
+        workerThread.interrupt();
+    }
+}
---
+++ app/src/main/java/de/ppluss/timingexperiments/WorkerThread.java
@@ -0,0 +1,92 @@
+package de.ppluss.timingexperiments;
+
+import android.content.Context;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Vibrator;
+
+public class WorkerThread extends Thread {
+
+    private final Context context;
+
+    public boolean shouldWork = false, singleRun = false;
+    public int mode = 0;
+    public int fireTime = 0, waitTime = 0;
+
+    private Vibrator vibrator;
+    private CameraManager cameraManager = null;
+    String cameraId;
+
+    public WorkerThread(Context context) {
+        this.context = context;
+
+        this.vibrator = (Vibrator) this.context.getSystemService(Context.VIBRATOR_SERVICE);
+    }
+
+    @Override
+    public void run() {
+        super.run();
+
+        try {
+            for(;;) {
+
+                work();
+
+                if(singleRun) {
+                    singleRun = false;
+                }
+
+                while(!shouldWork && !singleRun) {
+                    sleep(10);
+                }
+
+            }
+        } catch (InterruptedException ignored) {}
+
+    }
+
+    public void stopEverything() {
+        vibrator.cancel();
+        if(cameraManager != null) {
+            try {
+                cameraManager.setTorchMode(cameraId, false);
+            } catch (CameraAccessException ignored) {}
+        }
+    }
+
+    private void work() throws InterruptedException {
+        if(shouldWork || singleRun) fire();
+        if(shouldWork) wait_();
+    }
+
+    private void fire() throws InterruptedException {
+
+        if(fireTime == 0) {
+            return;
+        }
+
+        if(mode == 0) {
+            vibrator.vibrate(fireTime);
+            sleep(fireTime);
+            vibrator.cancel();
+        } else if (mode == 1) {
+            try {
+                if(cameraManager == null) {
+                    cameraManager = (CameraManager) context.getSystemService(Context.CAMERA_SERVICE);
+                    cameraId = cameraManager.getCameraIdList()[0];
+                }
+                cameraManager.setTorchMode(cameraId, true);
+                sleep(fireTime);
+                cameraManager.setTorchMode(cameraId, false);
+            } catch (CameraAccessException | NullPointerException ignored) {}
+        }
+
+    }
+
+    private void wait_() throws InterruptedException {
+        if(waitTime != 0) {
+            sleep(waitTime);
+        }
+    }
+
+}
